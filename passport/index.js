d5e0e583550623103dd9ad51690d01e63b092253
Radhika <radhikaiyer@Radhikas-MacBook-Air.local>
2018-07-07 12:38:50 -0700

    Adding index and passport files

---
+++ passport/index.js
@@ -0,0 +1,8 @@
+const passport = require('passport');
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+
+const GoogleCreds = {
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: process.env.GOOGLE_CB_URL
+  }
