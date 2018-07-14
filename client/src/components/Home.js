3df0b5fbe001b4c6cf63bed5818ebd1e291f6b84
EmmaNem <emmabv1@gmail.com>
2018-07-13 20:18:52 -0700

    styles

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -4,8 +4,10 @@ import "./login.css";
 
 const Home = () => {
     return (
-        <div>
-            <p>Welcome (name) to Potlucky</p>
+        <div className="container">
+            <div className="title">
+                <h2>Hi (name)!</h2>
+            </div>
             <div>
             <NavLink to="/events"><button className="events">Upcoming Events</button></NavLink>
             <NavLink to="/newevent"><button className="create">Create Event</button></NavLink>
