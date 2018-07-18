f3c9bd6049027253f06dc579af842a4b62e74160
Radhika <radhikaiyer@Radhikas-MacBook-Air.local>
2018-07-17 19:15:42 -0700

    working google auth

---
+++ authRoutes.js
@@ -0,0 +1,8 @@
+const router = require('express').Router();
+const passport = require('../passport');
+
+router.get('/google', passport.authenticate('google', {
+    scope: ["profile", "email"]
+  }))
+
+
