be05031d82965797e93b4a7b0d95f4e830ee939c
Meghna <meghnanag16@gmail.com>
2018-07-19 18:08:42 -0700

    forcetrue

--- server.js
+++ server.js
@@ -176,7 +176,7 @@ app.get('/auth/google/callback',
   });
 
 
-db.sequelize.sync().then(function() {
+db.sequelize.sync({force:true}).then(function() {
   app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
