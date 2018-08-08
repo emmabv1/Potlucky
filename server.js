f09d14b75d62f09d199967de65d72dbb00f5c435
EmmaNem <emmabv1@gmail.com>
2018-08-07 18:24:18 -0700

    server

--- server.js
+++ server.js
@@ -165,7 +165,7 @@ app.get('/auth/google/callback',
 db.sequelize.sync().then(function() {
 //db.sequelize.sync({force:true}).then(function() {
 
-  app.listen(8000, function() {
+  app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
 });
